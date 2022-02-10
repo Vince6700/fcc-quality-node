c5b615a6de05389a3493dc78133d666fd358f066
Vincent Racelle <vincentracelle@MacBook-Pro-de-Vince.local>
2022-02-10 12:48:58 +0100

    functinal front testing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -63,6 +63,12 @@ suite("Functional Tests", function () {
 });
 
 const Browser = require("zombie");
+Browser.site = "http://localhost:3000/";
+const browser = new Browser();
+
+suiteSetup(function (done) {
+  return browser.visit("/", done);
+});
 
 suite("Functional Tests with Zombie.js", function () {
   this.timeout(5000);
@@ -76,13 +82,27 @@ suite("Functional Tests with Zombie.js", function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", "Colombo").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.status(200);
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.elements("span#dates", 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill("surname", "Vespucci").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.status(200);
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.elements("span#dates", 1);
+          done();
+        });
+      });
 
       done();
     });
